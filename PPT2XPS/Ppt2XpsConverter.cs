1a1d246c46a6e075e03f1dc821c7472da393ddd0
Luis Cantero <luiscantero@hotmail.com>
2015-09-20 21:51:15 +0200

    Initial

---
+++ PPT2XPS/Ppt2XpsConverter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using MSOInterop = Microsoft.Office.Interop;
+using MSOCore = Microsoft.Office.Core;
+
+namespace PPT2XPS
+{
+    public static class Ppt2XpsConverter
+    {
+        public static void PrintFileAsXps(string path, int index, int total)
+        {
+            // Avoid Quit if already running.
+            bool appAlreadyRunning = System.Diagnostics.Process.GetProcessesByName("powerpnt").Length > 0;
+
+            // Create/get instance of MS PowerPoint.
+            Console.WriteLine("Starting MS PowerPoint ...");
+            var app = new MSOInterop.PowerPoint.Application();
+
+            // Open presentation.
+            MSOInterop.PowerPoint._Presentation pptFile;
+
+            var fileInfo = new FileInfo(path);
+            Console.WriteLine($"Opening {Path.GetFileName(path)} ({(double)fileInfo.Length / 1024 / 1024:0.##} MB) {index}/{total} ...");
+            pptFile = app.Presentations.Open(path,
+                                             WithWindow: MSOCore.MsoTriState.msoFalse); // Don't show window.
+
+            // Set options.
+            SetPrintOptions(pptFile);
+
+            // Delete if exists.
+            DeleteIfExists(pptFile);
+
+            // Print.
+            Console.WriteLine($"Printing ...");
+            pptFile.PrintOut(PrintToFile: pptFile.FullName.Replace(".pptx", ".xps"));
+
+            // Close file.
+            pptFile.Close();
+            Marshal.ReleaseComObject(pptFile);
+            pptFile = null;
+
+            // Quit app.
+            if (!appAlreadyRunning)
+            {
+                app.Quit();
+            }
+
+            // Force process to exit.
+            Marshal.ReleaseComObject(app);
+            app = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        private static void DeleteIfExists(MSOInterop.PowerPoint._Presentation pptFile)
+        {
+            string xpsPath = pptFile.FullName.Replace(".pptx", ".xps");
+            if (File.Exists(xpsPath))
+            {
+                Console.WriteLine($"Deleting existing file ...");
+                File.Delete(xpsPath);
+            }
+        }
+
+        private static void SetPrintOptions(MSOInterop.PowerPoint._Presentation pptFile)
+        {
+            pptFile.PrintOptions.ActivePrinter = "Microsoft XPS Document Writer";
+            pptFile.PrintOptions.OutputType = MSOInterop.PowerPoint.PpPrintOutputType.ppPrintOutputNotesPages;
+            pptFile.PrintOptions.HighQuality = MSOCore.MsoTriState.msoFalse; // msoTrue = -1 | msoFalse = 0
+            pptFile.PrintOptions.PrintColorType = MSOInterop.PowerPoint.PpPrintColorType.ppPrintColor;
+            pptFile.PrintOptions.PrintHiddenSlides = MSOCore.MsoTriState.msoTrue;
+            pptFile.PrintOptions.PrintInBackground = MSOCore.MsoTriState.msoFalse; // Wait for print to finish.
+        }
+    }
+}
---
+++ PPT2XPS/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.IO;
+using System.Diagnostics;
+
+namespace PPT2XPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use path from 1st argument or exe location.
+            string path = GetParamOrExePath(args.FirstOrDefault());
+
+            PrintAll(path);
+        }
+
+        private static string GetParamOrExePath(string paramPath)
+        {
+            return paramPath?.ToString() ?? Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        private static void PrintAll(string path)
+        {
+            Stopwatch watch = Stopwatch.StartNew();
+
+            string[] files = GetFiles(path, "*.pptx");
+
+            if (files.Length > 0)
+            {
+                PrintFiles(files);
+
+                Console.WriteLine("Done converting PPTX to XPS!");
+
+                // Time taken.
+                watch.Stop();
+                TimeSpan ts = watch.Elapsed;
+                Console.WriteLine($"Total time taken: {ts.Minutes:00}:{ts.Seconds:00}");
+            }
+            else
+            {
+                watch.Stop();
+                Console.WriteLine("No files to convert.");
+            }
+        }
+
+        private static string[] GetFiles(string path, string searchPattern)
+        {
+            Console.WriteLine($"Listing {searchPattern} files in {path} ...");
+            Console.WriteLine("");
+            string[] files = Directory.GetFiles(path, searchPattern);
+
+            return files;
+        }
+
+        private static void PrintFiles(string[] files)
+        {
+            for (int i = 0; i < files.Length; i++)
+            {
+                Stopwatch watch = Stopwatch.StartNew();
+
+                try
+                {
+                    // Print file.
+                    Ppt2XpsConverter.PrintFileAsXps(files[i], i + 1, files.Length);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+
+                // Time taken.
+                watch.Stop();
+                TimeSpan ts = watch.Elapsed;
+                Console.WriteLine($"Time taken: {ts.Minutes:00}:{ts.Seconds:00}");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
